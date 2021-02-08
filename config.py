8b1a19197d1bfdf6f5855c340fe4e4f98d82432f
SeerLite <seerlite@tuta.io>
2021-02-07 22:04:27 -0300

    Remove channel from config

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ config = configparser.ConfigParser()
 config["server"] = {}
 config["bot"] = {}
 
-config["server"]["host"] = input("(1/4) IP: ")
-config["server"]["port"] = input("(2/4) Port: ")
-config["server"]["channel"] = input("(3/4) Channel: ")
-config["bot"]["username"] = input("(4/4) Bot username: ")
+config["server"]["host"] = input("(1/3) IP: ")
+config["server"]["port"] = input("(2/3) Port: ")
+config["bot"]["username"] = input("(3/3) Bot username: ")
 
 with open("configuration.ini", "w") as config_file:
     config.write(config_file)
